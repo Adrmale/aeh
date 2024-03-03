23ffef0a685537fdce4d96c28db9a620cd36a350
Adrian <adrmalec@proton.me>
2024-03-03 20:55:13 +0100

    zmiana-skryptu

--- master.java
+++ master.java
@@ -2,7 +2,9 @@ public class master {
     public static void main(String[] args) {
       String text = "Adrian";
       Integer lata = 16;
-      System.out.println(text +" "+ "ma" +" "+ lata +" "+ "lat");
+      String textt = "Malec";
+      System.out.println(text+ " "+ textt +" "+ "ma" +" "+ lata +" "+ "lat");
+      
     }
   }
 
