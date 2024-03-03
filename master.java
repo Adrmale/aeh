3dc4c3e3403d9e83b702538990a131f6c40b8f68
Adrian <adrmalec@proton.me>
2024-03-03 15:19:09 +0100

    poprawa-bledow

--- master.java
+++ master.java
@@ -1,5 +1,8 @@
 public class master {
     public static void main(String[] args) {
-      System.out.println("Hello Worlder");
+      String text = "Adrian";
+      Integer lata = 16;
+      System.out.println(text+ "ma" + lata + "lat");
     }
   }
+
