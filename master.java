5493bebada4149ef1e68f4ab2a7ff1d5d6c2017e
Adrian <adrmalec@proton.me>
2024-03-03 15:23:09 +0100

    dopisanie-kodu

--- master.java
+++ master.java
@@ -2,7 +2,7 @@ public class master {
     public static void main(String[] args) {
       String text = "Adrian";
       Integer lata = 16;
-      System.out.println(text+ "ma" + lata + "lat");
+      System.out.println(text +" "+ "ma" +" "+ lata +" "+ "lat");
     }
   }
 
