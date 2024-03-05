c95d2b6be435209b66aafb38bce1dea0fbe1f773
Adrmale <adrmalec@proton.me>
2024-03-05 14:27:45 +0100

    chore:cwiczenie

--- master.java
+++ master.java
@@ -1,5 +1,22 @@
 public class master {
     public static void main(String[] args) {
-        System.out.println("Hello Worlds");
-    }
+    //     int liczba1 = 12;
+    //     int mynum = 1;
+       
+    //     String zmienna1;
+    //     zmienna1 = "AEH";
+    //     System.out.println(zmienna1);
+
+    //     System.out.println(liczba1);
+    //     System.out.println(mynum);
+    //     System.out.println("Hello Worlds");
+    // }
+    System.out.println(add(5, 5));
+
+}
+
+public static int add(int a, int b) {
+    int sum = a + b;
+    return sum;
+}
 }
