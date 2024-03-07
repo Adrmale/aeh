c9eab41666932dd72419005fb57a772b524e6d75
Adrian <adrmalec@proton.me>
2024-03-07 16:36:14 +0100

    cwiczenie

--- master.java
+++ master.java
@@ -1,10 +1,70 @@
 public class master {
     public static void main(String[] args) {
-      String text = "Adrian";
-      Integer lata = 16;
-      String textt = "Malec";
-      System.out.println(text+ " "+ textt +" "+ "ma" +" "+ lata +" "+ "lat");
+      // String text = "Adrian";
+      // Integer lata = 16;
+      // String textt = "Malec";
+      // System.out.println(text+ " "+ textt +" "+ "ma" +" "+ lata +" "+ "lat");
       
+      // int numer = 10;
+      // if(numer > 0) {
+      //   System.out.println("pozytywne");
+      // }
+      // else{
+      //   System.out.println("negatywne");
+      // }
+
+        // int numer = 10;
+        // if(numer > 0){
+        //   System.out.println("Liczba" + " " + numer + " " + "jest większe od zera");
+        // } else if(numer < 0) {
+        //   System.out.println("Liczba" + " " + numer + " " + "jest mniejsza od zera");
+        // } else {
+        //   System.out.println("Liczba" + " " + numer + " " + "jest równa zero");
+        // }
+
+        // int weekday = 0;
+        // switch (weekday) {
+        //   case 1:
+        //     System.out.println("Monday");
+        //     break;
+        //   case 2:
+        //     System.out.println("Tuesday");
+        //     break;
+        //   case 3:
+        //     System.out.println("Wednesday");
+        //     break;
+        //   case 4:
+        //     System.out.println("Thursday");
+        //     break;
+        //   case 5:
+        //     System.out.println("Friday");clea
+        //     break;
+        //   default:
+        //   System.out.println("Weekend!");
+            
+        // }
+        
+        // int number = 6;
+        // String wynik = (number % 2 == 0) ? "even" : "odd";
+        // System.out.println("Number is " + wynik);
+
+
+        // for (int i = 15; i<= 30; i++){
+        //   System.out.println(i);
+        // }
+
+        // int i = 10;
+        // while (i <= 25) {
+        //   System.out.println(i);
+        //   i++;
+        // }
+        
+        // int j = 1;
+        // do{
+        //   System.out.println(j);
+        //   j++;
+        // }while (j <= 5);
     }
   }
 
+ 
